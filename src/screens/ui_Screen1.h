23e5181637ae45800f816ab0db70b5ce6f7076b9
Łukasz Jakubek <lukasz.jakubek@gmail.com>
2026-09-22 17:47:52 +0200

    Removed Panel1 from Screen1

--- src/screens/ui_Screen1.h
+++ src/screens/ui_Screen1.h
@@ -21,7 +21,6 @@ extern lv_obj_t * ui_Humidity;
 extern lv_obj_t * ui_Humidity_history;
 extern void ui_event_Button_to_screen2(lv_event_t * e);
 extern lv_obj_t * ui_Button_to_screen2;
-extern lv_obj_t * ui_Panel1;
 // CUSTOM VARIABLES
 extern lv_obj_t * uic_Screen1;
 extern lv_obj_t * uic_Temperature;
